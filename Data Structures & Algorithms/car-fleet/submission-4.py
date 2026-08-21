cbe3661ca34b4cc5a36408bd208f2d75b313768c
Alex Delistathis <115193720+alex-delistathis@users.noreply.github.com>
2026-08-21 16:06:43 -0400

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        length = len(position)
+        sortedPosition, sortedSpeed = zip(*sorted(zip(position, speed)))
+        sortedPosition = list(sortedPosition)
+        sortedSpeed = list(sortedSpeed)
+        fleets = 0
+        currentFleet = [(target - sortedPosition[length - 1]) / sortedSpeed[length - 1], [length - 1]]
+        i = length - 2
+        while i >= 0:
+            currentTime = (target - sortedPosition[i]) / sortedSpeed[i]
+            fleetTime = currentFleet[0]
+            if currentTime <= fleetTime:
+                currentFleet[1].append(i)
+                currentFleet[0] = fleetTime
+            else:
+                fleets += 1
+                currentFleet = [currentTime, [i]]
+            i -= 1
+    
+        if currentFleet:
+            fleets += 1
+            currentFleet = []
+
+        return fleets
