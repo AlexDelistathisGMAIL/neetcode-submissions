c4bc183e3dde539c6e3d1bc607e37ad1612800f5
Alex Delistathis <115193720+alex-delistathis@users.noreply.github.com>
2026-08-23 21:49:27 -0400

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        first = 0
+        last = len(nums) - 1
+        middle = (first + last) // 2
+        while first <= last:
+            candidate = nums[middle]
+            if candidate == target:
+                return middle
+            elif candidate < target:
+                first = middle
+            else:
+                last = middle
+            middle = (first + last) // 2
+            if middle == first or middle == last:
+                break
+            
+        return -1
---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        length = len(nums)
+        sortedNums = sorted(nums)
+        triplets = []
+        i = 0
+        while i < length:
+            if nums[i] > 0:
+                i += 1
+                continue
+            j = i + 1
+            k = length - 1
+            while j < k:
+                currentSum = sortedNums[i] + sortedNums[j] + sortedNums[k]
+                if currentSum == 0:
+                    triplet = sortedNums[i], sortedNums[j], sortedNums[k]
+                    if triplet not in triplets:
+                        triplets.append(triplet)
+                    j += 1
+                elif currentSum < 0:
+                    j += 1
+                else:
+                    k -= 1
+            i += 1
+        
+        return triplets
